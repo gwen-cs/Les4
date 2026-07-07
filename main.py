a03bfab521488aac5ca32ad5155ba6e26533d248
Gwen Reynders <gwenneke@pm.me>
2026-07-07 20:10:43 +0200

    form gemaakt en uitgeprint

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from dotenv import load_dotenv
+from markupsafe import escape
 import os
 
 app = Flask(__name__)
@@ -9,3 +8,14 @@ app = Flask(__name__)
 @app.route ("/")
 def main ():
     return render_template("index.html")
+
+@app.route ("/send")
+def send ():
+    return render_template("send.html")
+
+@app.route ("/process-data", methods = ["POST"])
+def process_data():
+    firstname = request.form.get("firstname")
+    age = int(request.form.get("age"))
+
+    return f"<p> Hello {escape(firstname)}, You are {age} years old. </p>"
